79f14049434397408af549838835419950b4530f
“caiquecso” <caique.oliveira@signainfo.com.br>
2025-09-01 11:33:42 -0300

    fix: TypeScript na requisição da API

--- src/services/offersService.ts
+++ src/services/offersService.ts
@@ -1,10 +1,12 @@
-export async function fetchOffers() {
+import { Offer } from "../types/offer";
+
+export async function fetchOffers(): Promise<Offer[]> {
   try {
     const response = await fetch("http://localhost:3000/offers");
     if (!response.ok) {
       throw new Error("Erro ao buscar ofertas");
     }
-    return await response.json();
+    return (await response.json()) as Offer[];
   } catch (error) {
     console.error(error);
     return [];
